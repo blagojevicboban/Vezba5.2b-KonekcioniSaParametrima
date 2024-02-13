28c5169e23d7eb3132d9a3310aef84b1e3242ec6
Boban Blagojević <boban.blagojevic@tsp.edu.rs>
2024-02-13 12:55:32 +0100

    Add project files.

---
+++ Form1.Designer.cs
@@ -0,0 +1,118 @@
+﻿namespace Vezba5._2b
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(33, 23);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(383, 67);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "SELECT * FROM Ucenici";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(452, 23);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(103, 67);
+            this.button1.TabIndex = 1;
+            this.button1.Text = "Konekcioni prikaz";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridView1.Location = new System.Drawing.Point(33, 120);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersWidth = 62;
+            this.dataGridView1.RowTemplate.Height = 28;
+            this.dataGridView1.Size = new System.Drawing.Size(744, 318);
+            this.dataGridView1.TabIndex = 2;
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.DropDownStyle = System.Windows.Forms.ComboBoxStyle.DropDownList;
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "2003",
+            "2004",
+            "2005"});
+            this.comboBox1.Location = new System.Drawing.Point(630, 52);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(655, 23);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(65, 20);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Godiste";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Data.SqlClient;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Vezba5._2b
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            try
+            {
+                konekcija.ConnectionString = 
+            (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SkolaDB.mdf;Integrated Security=True");
+                konekcija.Open();
+                SqlCommand komanda = new SqlCommand
+                    (textBox1.Text, konekcija);
+                DataTable tabela = new DataTable();
+                SqlDataReader citac = komanda.ExecuteReader();
+                tabela.Load(citac);
+                dataGridView1.DataSource = tabela;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+            SqlConnection konekcija = new SqlConnection();
+            try
+            {
+                konekcija.ConnectionString =
+            (@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=|DataDirectory|\SkolaDB.mdf;Integrated Security=True");
+                konekcija.Open();
+                string upit = "SELECT * FROM Ucenici WHERE Generacija=" 
+                    + comboBox1.Text;
+                // prenos parametara
+                SqlParameter parGod = new SqlParameter();
+                parGod.ParameterName = "@Generacija1";
+                parGod.Value = comboBox1.Text;
+                string parupit =
+                    "SELECT * FROM Ucenici WHERE Generacija=@Generacija2";
+                SqlCommand komanda = new SqlCommand
+                    (parupit, konekcija);
+                // 1- nacin
+                komanda.Parameters.Add(parGod); 
+                // 2- nacin
+                komanda.Parameters.AddWithValue("@Generacija2", comboBox1.Text);
+                DataTable tabela = new DataTable();
+                SqlDataReader citac = komanda.ExecuteReader();
+                tabela.Load(citac);
+                dataGridView1.DataSource = tabela;
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                konekcija.Close();
+            }
+        }
+    }
+}
